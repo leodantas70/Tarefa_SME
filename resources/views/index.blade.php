ccb3e7b264a5036ef2466912e5e87504f444a2f3
Leo Dantas <leodantas70@gmail.com>
2023-12-19 21:26:49 -0300

    Adicionado botão para o painel de administrador

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,17 +2,25 @@
 @section ('body')
 <h1 class="text-center"> Bem vindo a Tarefa_SME </h1>
     <div class="text-center">
-    <a href="{{ url ('ordem') }} ">  <br/>
+    <a href="{{ url ("ordem") }} ">  <br/>
         <button  type="submit" class="btn btn-primary">Abrir Chamado</button><br/>  
     </a>
-    </div>
+    </div>   
 
-    
-        <div class="container-fluid"><br/><br/>
+    <div class="container-fluid"><br/><br/>
             <h2 class="text-center display-4">Pesquisar Chamado</h2>
             <div class="text-center">
-                <a href="{{url("mostrarordem")}}">  <br/>
+                <a href="{{ url ("mostrarordem") }}">
                     <button  type="submit" class="btn btn-primary">Pesquisar Chamado</button><br/>  
                 </a>
-                </div>
+            </div>
+    </div>
+    <div class="container-fluid"><br/><br/>
+        <h2 class="text-center display-4">Painel Admin</h2>
+        <div class="text-center">
+            <a href="{{ url ("login") }}">
+                <button  type="submit" class="btn btn-primary">Entrar</button><br/>  
+            </a>
+        </div>
+    </div>
 @endsection
